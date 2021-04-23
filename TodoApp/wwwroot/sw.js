90d98af6124b9346bccbc358f70d2140a51be347
Dave Nunez <noonz2@icloud.com>
2021-04-23 12:30:47 -0400

    create service worker, precache index,css, and js file | make app installable

---
+++ TodoApp/wwwroot/sw.js
@@ -0,0 +1,30 @@
+﻿const cacheName = 'Todo-cache';
+
+function swInstall(event) {
+    event.waitUntil(
+        caches.open(cacheName).then((cache) => {
+            return cache.addAll([
+                './',
+                './todo/index',
+                './home/index',
+                './css/site.css',
+                './js/site.js'
+            ]);
+        })
+    );
+}
+self.addEventListener('install', swInstall);
+
+self.addEventListener('fetch', function (event) {
+    event.respondWith(
+        fetch(event.request)
+            .then(function (networkResponse) {
+                caches.open(cacheName).then((cache) => {
+                    cache.add(event.request.url);
+                });
+                return networkResponse
+            }).catch(function () {
+                return caches.match(event.request)
+            })
+    )
+})
