1f5d54ff2a9353344dd6d16eb9ed4bb44e571d28
Dave Nunez <noonz2@icloud.com>
2021-04-23 18:12:12 -0400

    added copy title to clipboard in index

--- TodoApp/wwwroot/js/site.js
+++ TodoApp/wwwroot/js/site.js
@@ -2,3 +2,20 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+if ('clipboard' in navigator) {
+    let canWrite = false;
+    navigator.permissions.query({ name: 'clipboard-write' }).then((perms) => {
+        canWrite = perms.state;
+
+        if (canWrite === "granted") {
+            $("#copyToClipboard").on('click', () => { navigator.clipboard.writeText(window.document.getElementById('dueTitle').innerHTML); })
+        } else if (canWrite === "prompt") {
+            $("#copyToClipboard").on('click', () => { $("#clipboardPerms").show(); })
+        } else {
+            return;
+        }
+
+        $("#copyToClipboard").removeAttr('hidden');
+    })
+}
